c5935ab905124bfd8b3d623f34bd1ddb928bfdae
Shahabaz Bagwan <4272320+Shahabaz-Bagwan@users.noreply.github.com>
2025-08-05 11:26:32 +0200

    add autocmds

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,5 +1,8 @@
 -- autocmd
 --------------------------------------------------------------------------------
+vim.api.nvim_create_autocmd({ "BufEnter" }, { pattern = { "*" }, command = "normal zx" })
+vim.api.nvim_create_autocmd({ "BufEnter" }, { pattern = { "*" }, command = "normal zR" })
+
 -- Highlight when yanking
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
@@ -7,4 +10,40 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
     vim.highlight.on_yank()
   end,
+})
+
+-- restore cursor pos on file open
+vim.api.nvim_create_autocmd("BufReadPost", {
+	pattern = "*",
+	callback = function()
+	local line = vim.fn.line("'\"")
+	if line > 1 and line <= vim.fn.line("$") then
+		vim.cmd("normal! g'\"")
+	end
+end,
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
+   pattern = "*",
+   group = augroup,
+   callback = function()
+      if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
+         vim.opt.relativenumber = true
+      end
+   end,
+})
+
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
+   pattern = "*",
+   group = augroup,
+   callback = function()
+      if vim.o.nu then
+         vim.opt.relativenumber = false
+         -- Conditional taken from https://github.com/rockyzhang24/dotfiles/commit/03dd14b5d43f812661b88c4660c03d714132abcf
+         -- Workaround for https://github.com/neovim/neovim/issues/32068
+         if not vim.tbl_contains({"@", "-"}, vim.v.event.cmdtype) then
+            vim.cmd "redraw"
+         end
+      end
+   end,
 })
