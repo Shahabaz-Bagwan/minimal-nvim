38dc0200cffbd461a2f692dac6e5b7b5b28ccc97
Shahabaz Bagwan <4272320+Shahabaz-Bagwan@users.noreply.github.com>
2025-08-05 11:27:28 +0200

    use mini completion

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,25 +1,35 @@
 vim.pack.add({
-  { src = 'https://github.com/echasnovski/mini.nvim', branch = 'stable' }
+	{ src = "https://github.com/echasnovski/mini.nvim", branch = "stable" },
 })
-require('mini.indentscope').setup(
-    { symbol = "│", options = { try_as_border = true }}
-)
-require('mini.icons').setup()
-require('mini.files').setup()
+require("mini.indentscope").setup({ symbol = "│", options = { try_as_border = true } })
+require("mini.icons").setup()
+
+require("mini.files").setup()
 
 vim.keymap.set("n", "<leader>e", function()
-  require("mini.files").open()
-end, {desc= "open mini files"})
+	local bufname = vim.api.nvim_buf_get_name(0)
+	local _ = require("mini.files").close() or require("mini.files").open(bufname, false)
+end, { desc = "File explorer" })
 
-require('mini.statusline').setup()
-require('mini.cursorword').setup()
+require("mini.statusline").setup()
+require("mini.cursorword").setup()
 
--- vim.pack.add({
---   { src = 'https://github.com/rafamadriz/friendly-snippets' },
---   { src = 'https://github.com/L3MON4D3/LuaSnip' },
--- })
+vim.pack.add({
+	{ src = "https://github.com/rafamadriz/friendly-snippets" },
+})
 
--- require('LuaSnip')
--- require('mini.snippets').setup()
-require('mini.ai').setup()
-require('mini.surround').setup()
+local gen_loader = require("mini.snippets").gen_loader
+require("mini.snippets").setup({
+	snippets = {
+		-- Load custom file with global snippets first (adjust for Windows)
+		gen_loader.from_file("~/.config/nvim/snippets/global.json"),
+
+		-- Load snippets based on current language by reading files from
+		-- "snippets/" subdirectories from 'runtimepath' directories.
+		gen_loader.from_lang(),
+	},
+})
+
+require("mini.completion").setup()
+require("mini.ai").setup()
+require("mini.surround").setup()
