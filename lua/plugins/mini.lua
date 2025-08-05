91102fdf70309a4581a0760a74e1b1e51c028d76
Shahabaz Bagwan <4272320+Shahabaz-Bagwan@users.noreply.github.com>
2025-08-05 12:03:08 +0200

    add extra mini modules and format with stylua

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,18 +2,71 @@ vim.pack.add({
 	{ src = "https://github.com/echasnovski/mini.nvim", branch = "stable" },
 })
 require("mini.indentscope").setup({ symbol = "│", options = { try_as_border = true } })
-require("mini.icons").setup()
-
-require("mini.files").setup()
-
-vim.keymap.set("n", "<leader>e", function()
-	local bufname = vim.api.nvim_buf_get_name(0)
-	local _ = require("mini.files").close() or require("mini.files").open(bufname, false)
-end, { desc = "File explorer" })
-
+require("mini.surround").setup()
+require("mini.ai").setup()
 require("mini.statusline").setup()
 require("mini.cursorword").setup()
+require("mini.pairs").setup()
+require("mini.notify").setup()
+-- require("mini.tabline").setup()
+require("mini.starter").setup()
+require("mini.pick").setup()
+require("mini.extra").setup()
 
+-- require("mini.clue").setup({
+-- 	window = {
+-- 		config = { anchor = "SW", row = "auto", col = "auto" },
+-- 	},
+-- 	triggers = {
+-- 		-- Leader triggers
+-- 		{ mode = "n", keys = "<Leader>" },
+-- 		{ mode = "x", keys = "<Leader>" },
+--
+-- 		-- Built-in completion
+-- 		{ mode = "i", keys = "<C-x>" },
+--
+-- 		-- `g` key
+-- 		{ mode = "n", keys = "g" },
+-- 		{ mode = "x", keys = "g" },
+--
+-- 		-- Marks
+-- 		{ mode = "n", keys = "'" },
+-- 		{ mode = "n", keys = "`" },
+-- 		{ mode = "x", keys = "'" },
+-- 		{ mode = "x", keys = "`" },
+--
+-- 		-- Registers
+-- 		{ mode = "n", keys = '"' },
+-- 		{ mode = "x", keys = '"' },
+-- 		{ mode = "i", keys = "<C-r>" },
+-- 		{ mode = "c", keys = "<C-r>" },
+--
+-- 		-- Window commands
+-- 		{ mode = "n", keys = "<C-w>" },
+--
+-- 		-- `z` key
+-- 		{ mode = "n", keys = "z" },
+-- 		{ mode = "x", keys = "z" },
+-- 	},
+--
+-- 	clues = {
+-- 		-- Enhance this by adding descriptions for <Leader> mapping groups
+-- 		require("mini.clue").gen_clues.builtin_completion(),
+-- 		require("mini.clue").gen_clues.g(),
+-- 		require("mini.clue").gen_clues.marks(),
+-- 		require("mini.clue").gen_clues.registers(),
+-- 		require("mini.clue").gen_clues.windows(),
+-- 		require("mini.clue").gen_clues.z(),
+-- 	},
+-- })
+
+----------------------------------------------------------------------
+require("mini.icons").setup()
+require("mini.icons").mock_nvim_web_devicons()
+require("mini.icons").tweak_lsp_kind()
+----------------------------------------------------------------------
+
+----------------------------------------------------------------------
 vim.pack.add({
 	{ src = "https://github.com/rafamadriz/friendly-snippets" },
 })
@@ -31,5 +84,13 @@ require("mini.snippets").setup({
 })
 
 require("mini.completion").setup()
-require("mini.ai").setup()
-require("mini.surround").setup()
+----------------------------------------------------------------------
+
+----------------------------------------------------------------------
+require("mini.files").setup()
+
+vim.keymap.set("n", "<leader>e", function()
+	local bufname = vim.api.nvim_buf_get_name(0)
+	local _ = require("mini.files").close() or require("mini.files").open(bufname, false)
+end, { desc = "File explorer" })
+----------------------------------------------------------------------
