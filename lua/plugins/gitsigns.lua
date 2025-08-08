b75ef36b4617cef0902f52f65752ea5dc211151d
Shahabaz Bagwan <4272320+Shahabaz-Bagwan@users.noreply.github.com>
2025-08-08 07:34:02 +0200

    add options to see git-blame

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,6 +2,7 @@ vim.pack.add({
 	"https://github.com/lewis6991/gitsigns.nvim",
 })
 require("gitsigns").setup({
+
 	signs = {
 		delete = { text = "󰍵" },
 		changedelete = { text = "󱕖" },
@@ -9,5 +10,37 @@ require("gitsigns").setup({
 		change = { text = "│" },
 		topdelete = { text = "‾" },
 		untracked = { text = "│" },
+	},
+	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+	numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+	linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+	word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+	watch_gitdir = {
+		interval = 1000,
+		follow_files = true,
+	},
+	auto_attach = true,
+	attach_to_untracked = true,
+	current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+	current_line_blame_opts = {
+		virt_text = true,
+		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+		delay = 1000,
+		ignore_whitespace = false,
+		virt_text_priority = 100,
+		use_focus = true,
+	},
+	current_line_blame_formatter = "<author>, <author_time>, <abbrev_sha> -> <summary>",
+	sign_priority = 6,
+	update_debounce = 100,
+	status_formatter = nil, -- Use default
+	max_file_length = 40000, -- Disable if file is longer than this (in lines)
+	preview_config = {
+		-- Options passed to nvim_open_win
+		border = "single",
+		style = "minimal",
+		relative = "cursor",
+		row = 1,
+		col = 1,
 	},
 })
