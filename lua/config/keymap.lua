89969bc2cb72939320c0e5312faee30945a502e1
Shahabaz Bagwan <4272320+Shahabaz-Bagwan@users.noreply.github.com>
2025-08-08 15:26:03 +0200

    make all keymap silent

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -2,21 +2,39 @@ local map = vim.keymap.set
 -- keymap
 --------------------------------------------------------------------------------
 -- Navigate visual lines
-map({ "n", "x" }, "j", "gj", { desc = "Navigate down (visual line)" })
-map({ "n", "x" }, "k", "gk", { desc = "Navigate up (visual line)" })
-map({ "n", "x" }, "<Down>", "gj", { desc = "Navigate down (visual line)" })
+map({ "n", "x" }, "j", "gj", { desc = "Navigate down (visual line)", silent = true })
+map({ "n", "x" }, "k", "gk", { desc = "Navigate up (visual line)", silent = true })
+map({ "n", "x" }, "<Down>", "gj", { desc = "Navigate down (visual line)", silent = true })
 
 map({ "n", "v" }, ";", ":", { desc = "Enter cmd" })
 
-map({ "n", "x" }, "<Up>", "gk", { desc = "Navigate up (visual line)" })
-map("i", "<Down>", "<C-\\><C-o>gj", { desc = "Navigate down (visual line)" })
+map({ "n", "x" }, "<Up>", "gk", { desc = "Navigate up (visual line)", silent = true })
+map("i", "<Down>", "<C-\\><C-o>gj", { desc = "Navigate down (visual line)", silent = true })
 
-map("i", "<Up>", "<C-\\><C-o>gk", { desc = "Navigate up (visual line)" })
+map("i", "<Up>", "<C-\\><C-o>gk", { desc = "Navigate up (visual line)", silent = true })
 
-map({ "n", "v" }, "j", 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', { desc = "Move down", expr = true })
-map({ "n", "v" }, "k", 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', { desc = "Move up", expr = true })
-map({ "n", "v" }, "Up", 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', { desc = "Move up", expr = true })
-map({ "n", "v" }, "Down", 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', { desc = "Move down", expr = true })
+map(
+	{ "n", "v" },
+	"j",
+	'v:count || mode(1)[0:1] == "no" ? "j" : "gj"',
+	{ desc = "Move down", silent = true, expr = true }
+)
+
+map({ "n", "v" }, "k", 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', { desc = "Move up", expr = true, silent = true })
+
+map(
+	{ "n", "v" },
+	"Up",
+	'v:count || mode(1)[0:1] == "no" ? "k" : "gk"',
+	{ desc = "Move up", expr = true, silent = true }
+)
+
+map(
+	{ "n", "v" },
+	"Down",
+	'v:count || mode(1)[0:1] == "no" ? "j" : "gj"',
+	{ desc = "Move down", expr = true, silent = true }
+)
 
 -- Move Lines
 map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move Line Down", silent = true })
@@ -57,8 +75,8 @@ map("n", "<leader>ca", "<cmd> %bd <bar> e# <bar> bd# <bar> '\" <CR>", { desc = "
 map("n", "<leader>x", "<cmd>bd<cr>", { desc = "buffer close", silent = true })
 
 -- Comment
-map("n", "<leader>/", "gcc", { desc = "toggle comment", remap = true })
-map("v", "<leader>/", "gc", { desc = "toggle comment", remap = true })
+map("n", "<leader>/", "gcc", { desc = "toggle comment", remap = true, silent = true })
+map("v", "<leader>/", "gc", { desc = "toggle comment", remap = true, silent = true })
 
 -- terminal
 map("t", "<C-x>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
