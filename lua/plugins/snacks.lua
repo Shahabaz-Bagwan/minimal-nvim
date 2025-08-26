ec74e79143280bb43114f9267a516d170b478a0b
Shahabaz Bagwan <4272320+Shahabaz-Bagwan@users.noreply.github.com>
2025-08-26 14:40:13 +0200

    fix(snacks): make keymaps oneliner

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -57,69 +57,65 @@ require("snacks").setup({
 
 -- keymaps
 local map = vim.keymap.set
-local picker = require("snacks.picker")
 
-map("n", "<leader>lg", function()
-	require("snacks").lazygit()
-end, { desc = "open lazygit" })
+map("n", "<leader>lg", ":lua Snacks.lazygit()<cr>", { desc = "open lazygit", silent = true })
 
-map("n", "<leader>tt", function()
-	require("snacks").terminal()
-end, { desc = "toggle terminal" })
+map("n", "<leader>tt", ":lua Snacks.terminal()<cr>", { desc = "toggle terminal", silent = true })
 
-map("n", "<leader>se", function()
-	picker.explorer()
-end, { desc = "open snacks explorer" })
+map("n", "<leader>se", ":lua Snacks.picker.explorer()<cr>", { desc = "open snacks explorer", silent = true })
 
-map("n", "<leader>gll", function()
-	picker.git_log_line()
-end, { desc = "show git line log" })
+map("n", "<leader>ff", ":lua Snacks.picker.projects()<cr>", { desc = "find folders", silent = true })
+
+map("n", "<leader>fw", ":lua Snacks.picker.grep()<cr>", { desc = "live grep", silent = true })
+
+map("n", "<leader>fa", ":lua Snacks.picker.smart()<cr>", { desc = "smart file search", silent = true })
+
+map("n", "<leader>fh", ":lua Snacks.picker.help()<cr>", { desc = "help page", silent = true })
+
+map("n", "<leader>ma", ":lua Snacks.picker.marks()<cr>", { desc = "find marks", silent = true })
+
+map("n", "<leader>fib", ":lua Snacks.picker.lines()<cr>", { desc = "find in current buffer", silent = true })
+
+map("n", "<leader>fb", ":lua Snacks.picker.buffers()<cr>", { desc = "find buffer", silent = true })
+
+map("n", "<leader>fo", ":lua Snacks.picker.recent()<cr>", { desc = "find from recent files", silent = true })
+
+map("n", "<leader>fg", ":lua Snacks.picker.registers()<cr>", { desc = "find registers", silent = true })
+
+map("n", "<leader>fr", ":lua Snacks.picker.resume()<cr>", { desc = "resume old search", silent = true })
+
+map("n", "<leader>gh", ":lua Snacks.picker.git_diff()<cr>", { desc = "git hunks", silent = true })
+
+map("n", "<leader>gfl", ":lua Snacks.picker.git_log_file()<cr>", { desc = "show git file log", silent = true })
+
+map("n", "<leader>gll", ":lua Snacks.picker.git_log_line()<cr>", { desc = "show git line log", silent = true })
 
 -- lsp
-map("n", "<leader>gd", function()
-	picker.lsp_definitions()
-end, { desc = "go to definition" })
+map("n", "<leader>gd", ":lua Snacks.picker.lsp_definitions()<cr>", { desc = "go to definition", silent = true })
 
-map("n", "<leader>gdi", function()
-	picker.diagnostics()
-end, { desc = "go to diagnostic" })
+map("n", "<leader>gdi", ":lua Snacks.picker.diagnostics()<cr>", { desc = "go to diagnostic", silent = true })
 
-map("n", "<leader>gD", function()
-	picker.lsp_declarations()
-end, { desc = "go to declaration" })
+map("n", "<leader>gD", ":lua Snacks.picker.lsp_declarations()<cr>", { desc = "go to declaration", silent = true })
 
-map("n", "<leader>gi", function()
-	picker.lsp_implementations()
-end, { desc = "go to implementation" })
+map("n", "<leader>gi", ":lua Snacks.picker.lsp_implementations()<cr>", { desc = "go to implementation", silent = true })
 
-map("n", "<leader>gr", function()
-	picker.lsp_references()
-end, { desc = "go to references" })
+map("n", "<leader>gr", ":lua Snacks.picker.lsp_references()<cr>", { desc = "go to references", silent = true })
 
-map("n", "<leader>gt", function()
-	picker.lsp_type_definitions()
-end, { desc = "go to type_definition" })
+map("n", "<leader>fk", ":lua Snacks.picker.keymaps()<cr>", { desc = "show all keymaps", silent = true })
 
-map("n", "<leader>gs", function()
-	picker.lsp_symbols()
-end, { desc = "find document_symbol" })
+map("n", "z=", ":lua Snacks.picker.spelling()<cr>", { desc = "show spelling suggestions", silent = true })
 
-map("n", "<leader>ws", function()
-	picker.lsp_workspace_symbols()
-end, { desc = "find workspace_symbol" })
+map("n", "<leader>gs", ":lua Snacks.picker.lsp_symbols()<cr>", { desc = "find document_symbol", silent = true })
 
-map("n", "<leader>fg", function()
-	picker.registers()
-end, { desc = "resume old search" })
-
-map("n", "<leader>fr", function()
-	picker.resume()
-end, { desc = "resume old search" })
-
-map("n", "<leader>fk", function()
-	picker.keymaps()
-end, { desc = "show all keymaps" })
-
-map("n", "z=", function()
-	picker.spelling()
-end, { desc = "show spelling suggestions" })
+map(
+	"n",
+	"<leader>gt",
+	":lua Snacks.picker.lsp_type_definitions()<cr>",
+	{ desc = "go to type_definition", silent = true }
+)
+map(
+	"n",
+	"<leader>ws",
+	":lua Snacks.picker.lsp_workspace_symbols()<cr>",
+	{ desc = "find workspace_symbol", silent = true }
+)
