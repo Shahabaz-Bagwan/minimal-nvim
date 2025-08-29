fc968998263a41f7d76d6fe839a92d2a30ae803a
Shahabaz Bagwan <4272320+Shahabaz-Bagwan@users.noreply.github.com>
2025-08-29 10:27:01 +0200

    fix(mini.nvim): update the link to mini-nvim org

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,5 +1,5 @@
 vim.pack.add({
-	{ src = "https://github.com/echasnovski/mini.nvim", branch = "stable" },
+	{ src = "https://github.com/nvim-mini/mini.nvim", branch = "stable" },
 })
 require("mini.surround").setup()
 require("mini.cursorword").setup()
